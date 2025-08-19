3b4d2758ee88c050bc28f9a54ec455eedae38e2e
ziad <zizogamal619@yahoo.com>
2025-08-19 20:21:33 +0300

    trial

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/World-time-/", // Adjust the base path if needed
+  base: "/World-time-", // Adjust the base path if needed
 });
